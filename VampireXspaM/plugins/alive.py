1595e28a9d6a4c99cdfc1c0864862264f8320d5f
Adityaaaa™ <92294112+Legendary-Aditya-xD@users.noreply.github.com>
2022-02-15 19:42:02 +0530

    Add files via upload

---
+++ VampireXspaM/plugins/alive.py
@@ -0,0 +1,155 @@
+from .. import Don, Don2, Don3, Don4, Don5 , Don6, Don7, Don8, Don9, Don10, SUDO_USERS, alwaysversion
+from .. import ALIVE_PIC
+from telethon import events, version, Button
+from telethon.tl.custom import button
+from time import time
+from datetime import datetime
+
+DON_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/4cc84cabf3ee789d64ce0.jpg"
+  
+
+          
+always = "**Tᴇᴀᴍ Aʟᴡᴀʏꜱ Sᴘᴀᴍ Bᴏᴛ Iꜱ Wᴏʀᴋɪɴɢ Pᴇʀꜰᴇᴄᴛʟʏ♠️♠️**\n\n"
+
+always += f"🔥**TᴇᴀᴍAʟᴡᴀʏꜱ Sᴘᴀᴍ Sʏꜱᴛᴇᴍ**🔥\n\n"
+
+always += f" **•⭕️• Pʏᴛʜᴏɴ Vᴇʀsɪᴏɴ** : `3.9.6`\n"
+
+always += f" **•⭕️• Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ** : `{version.__version__}`\n"
+
+always += f" **•⭕️• Bot Vᴇʀsɪᴏɴ**  : `{alwaysversion}`\n"
+
+always += f" **•⭕️• Cʀᴇᴀᴛᴏʀ**  : [Dᴏɴ](t.me/Always_Don)\n\n"
+         
+                                    
+@Don.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+    
+@Don2.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don2.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+
+@Don3.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don3.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+    
+@Don4.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don4.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+    
+@Don5.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don5.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+   
+@Don6.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don6.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+    
+@Don7.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don7.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+    
+@Don8.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don8.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+    
+    
+@Don9.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don9.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+    
+@Don10.on(events.NewMessage(pattern=".alive"))
+async def alive(event):
+    if event.sender_id in SUDO_USERS:
+     await Don10.send_file(event.chat_id,
+                                  DON_PIC,
+                                  caption=always,
+                                  buttons=[
+        [
+        Button.url("Cᴏᴍᴍᴜɴɪᴛʏ", "https://t.me/Team_Always")
+        ]
+        ]
+        )
+    
---
+++ VampireXspaM/plugins/help.py
@@ -0,0 +1,153 @@
+from .. import Don, Don2, Don3, Don4, Don5 , Don6, Don7, Don8, Don9, Don10, SUDO_USERS
+from telethon import events, Button
+from telethon.tl.custom import button
+from time import time
+from datetime import datetime
+    
+HELP_PIC = "https://telegra.ph/file/4cc84cabf3ee789d64ce0.jpg"
+
+Don_Help = "🔥 Tᴇᴀᴍ Aʟᴡᴀʏꜱ 🔥\n\n"
+ 
+
+Don_Help += f" ↧ 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙲𝙼𝙳𝚂 ↧\n\n"
+
+Don_Help += f" `.ping` - to check ping\n `.alive` - to check bot alive/version \n .`reboot` - to restart all spam bots\n\n"
+ 
+Don_Help += f" ↧ 𝙻𝙴𝙰𝚅𝙴 𝙲𝙼𝙳 ↧\n\n"
+
+Don_Help += f" `.bleave` - to leave public/private channel/groups\n\n"
+ 
+Don_Help += f" ↧ 𝚂𝙿𝙰𝙼 𝙲𝙼𝙳𝚂 ↧\n\n"
+
+Don_Help += f" `.fuk` - to raid\n `.hardcore` - to active reply raid\n `.dhardcore` - to de-active reply raid\n `.bspam` - this cmd use for Normal spam\n `.bigbspam` - this cmd use for big spam\n `.uspam` - this cmd use for unlimited Spam until You restart the bots!!\n `.dspam` - this cmd use for delay spam\n\n"
+
+
+@Don.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
+
+@Don2.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don2.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
+  
+@Don3.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don3.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
+
+@Don4.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don4.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
+  
+@Don5.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don5.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
+
+@Don6.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don6.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
+    
+@Don7.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don7.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
+    
+@Don8.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don8.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
+    
+@Don9.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don9.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
+   
+@Don10.on(events.NewMessage(pattern=".help"))
+async def help(event):
+    if event.sender_id in SUDO_USERS:
+     await Don10.send_file(event.chat_id,
+                                  HELP_PIC,
+                                  caption=Don_Help,
+                                  buttons=[
+        [
+        Button.url("ᴄʀᴇᴀᴛᴏʀ", "https://t.me/Always_Don")
+        ]
+        ]
+        )                      
