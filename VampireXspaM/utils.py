1871e11572a9140242885a684438a42b7347607d
Adityaaaa™ <92294112+Legendary-Aditya-xD@users.noreply.github.com>
2022-02-18 14:12:31 +0530

    Update utils.py

--- VampireXspaM/utils.py
+++ VampireXspaM/utils.py
@@ -8,13 +8,13 @@ import re
 
 def load_plugins(plugin_name):
     path = Path(f"VampireXspaM/plugins/{plugin_name}.py")
-    name = "TeamAlwaysXSpam.plugins.{}".format(plugin_name)
+    name = "VampireXspaM.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["VampireXspaM.plugins." + plugin_name] = load
-    print("TeamAlways X has Imported " + plugin_name)
+    print("VampireXspaM has Imported " + plugin_name)
 
 async def edit_or_reply(event, text):
     if event.sender_id in SUDO_USERS:
